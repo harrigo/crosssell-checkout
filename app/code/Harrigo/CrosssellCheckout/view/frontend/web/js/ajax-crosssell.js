4a9c0b1fbff12e44fb45a16602b63682b85e5c7c
Harri <Harri.g.owen@gmail.com>
2017-05-11 14:02:19 +0100

    Update ajax-crosssell.js

--- app/code/Harrigo/CrosssellCheckout/view/frontend/web/js/ajax-crosssell.js
+++ app/code/Harrigo/CrosssellCheckout/view/frontend/web/js/ajax-crosssell.js
@@ -1,5 +1,17 @@
-require(['jquery'],function($){
+require(["jquery", "domReady!"], function($){
+	function waitForElement(elementPath, callBack){
+	  window.setTimeout(function(){
+		if($(elementPath).length){
+		  callBack(elementPath, $(elementPath));
+		}else{
+		  waitForElement(elementPath, callBack);
+		}
+	  },500)
+	}
+	
    $.get('/crosssell/index/crosssell',function(response){
-      $("#crosssellblock").html(response);
-   })
-})
+	   waitForElement("#crosssellblock",function(){
+			$("#crosssellblock").html(response);
+		});
+	   })
+});
