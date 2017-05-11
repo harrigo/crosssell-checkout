8547401a2babc8ae5c24587f6fd366712e075d39
Harri <Harri.g.owen@gmail.com>
2017-05-11 08:52:50 +0100

    Add files via upload

---
+++ app/code/Harrigo/CrosssellCheckout/Controller/Index/Crosssell.php
@@ -0,0 +1,38 @@
+<?php
+namespace Harrigo\CrosssellCheckout\Controller\Index;
+
+use Magento\Framework\Controller\ResultFactory;
+
+class Crosssell extends \Magento\Framework\App\Action\Action
+{
+	protected $resultPageFactory;
+
+	/**
+	 * Constructor
+	 * 
+	 * @param \Magento\Framework\App\Action\Context  $context
+	 * @param \Magento\Framework\View\Result\PageFactory $resultPageFactory
+	 */
+	public function __construct(
+		\Magento\Framework\App\Action\Context $context,
+		\Magento\Framework\View\Result\PageFactory $resultPageFactory
+
+	)
+	{
+		$this->resultPageFactory = $resultPageFactory;
+		$this->_resultFactory = $context->getResultFactory();
+		parent::__construct($context);
+	}
+
+	/**
+	 * Execute view action
+	 * 
+	 * @return \Magento\Framework\Controller\ResultInterface
+	 */
+	public function execute()
+	{
+		echo "Crosssell Checkout";
+		$resultLayout = $this->resultFactory->create(ResultFactory::TYPE_LAYOUT);
+		return $resultLayout;
+	}
+}
